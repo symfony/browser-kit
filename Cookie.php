5b8564c882ca8eb9a06ed2840abc9b2a40f1e12a
Alexander M. Turek <me@derrabus.de>
2025-12-12 08:51:57 +0100

    [BrowserKit] Allow Cookie expiration to be an int

--- Cookie.php
+++ Cookie.php
@@ -48,17 +48,17 @@ class Cookie
     /**
      * Sets a cookie.
      *
-     * @param string      $name         The cookie name
-     * @param string|null $value        The value of the cookie
-     * @param string|null $expires      The time the cookie expires
-     * @param string|null $path         The path on the server in which the cookie will be available on
-     * @param string      $domain       The domain that the cookie is available
-     * @param bool        $secure       Indicates that the cookie should only be transmitted over a secure HTTPS connection from the client
-     * @param bool        $httponly     The cookie httponly flag
-     * @param bool        $encodedValue Whether the value is encoded or not
-     * @param string|null $samesite     The cookie samesite attribute
+     * @param string          $name         The cookie name
+     * @param string|null     $value        The value of the cookie
+     * @param string|int|null $expires      The time the cookie expires
+     * @param string|null     $path         The path on the server in which the cookie will be available on
+     * @param string          $domain       The domain that the cookie is available
+     * @param bool            $secure       Indicates that the cookie should only be transmitted over a secure HTTPS connection from the client
+     * @param bool            $httponly     The cookie httponly flag
+     * @param bool            $encodedValue Whether the value is encoded or not
+     * @param string|null     $samesite     The cookie samesite attribute
      */
-    public function __construct(string $name, ?string $value, ?string $expires = null, ?string $path = null, string $domain = '', bool $secure = false, bool $httponly = true, bool $encodedValue = false, ?string $samesite = null)
+    public function __construct(string $name, ?string $value, string|int|null $expires = null, ?string $path = null, string $domain = '', bool $secure = false, bool $httponly = true, bool $encodedValue = false, ?string $samesite = null)
     {
         if ($encodedValue) {
             $this->rawValue = $value ?? '';
